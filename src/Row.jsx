6a36c359c931d61192dd8d316729f69a996e2703
parham.zahedi <parhamcodgamer333@gmail.com>
2025-06-13 11:41:56 +0330

    Create Row.jsx

---
+++ src/Row.jsx
@@ -0,0 +1,19 @@
+import "./Row.css";
+export default function Row(props){
+    
+    return(
+        <>
+        <tr>
+<td>{props.Name}</td>
+<td>{props.lastname}</td>
+<td>{props.phone}</td>
+<td>{props.age}</td>
+
+
+
+
+
+        </tr>
+        </>
+    )
+}
